e2d6812015d724efa0af4ea59d668a8dcfbb0458
Chiranjeevi <jeevi146@gmail.com>
2018-04-05 20:02:10 -0400

    Add auth routes skeleton

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@
 
 const express = require('express');
 const app = express();
+const authRoutes = require('./routes/auth-routes');
+
+//setup route for /auth/*
+app.use('/auth', authRoutes);
 
 //setup view engine
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
-
 
 app.get('/', (req, res) => {
   res.render('home');
---
+++ src/routes/auth-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+//auth login
+router.get('/login', (req, res) => {
+  res.render('login'); //renders login page in views dir
+});
+
+//auth logout
+router.get('/logout', (req, res) => {
+  //handle with passport.js
+  res.send('Logging out');
+});
+
+//auth with google
+router.get('/google', (req, res) => {
+  //handle with passport.js
+  res.send('Logging in with Google');
+});
+
+module.exports = router;
